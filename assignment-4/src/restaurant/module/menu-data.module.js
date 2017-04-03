ecb5ffff85f8069abe0a372bd84e9882b9ca9d2b
Ernesto Flores <ernesto.flores@ihg.com>
2017-04-03 12:25:33 -0500

    Assignment 4

---
+++ assignment-4/src/restaurant/module/menu-data.module.js
@@ -0,0 +1,7 @@
+(function () {
+'use strict';
+
+// Module to manage data services
+angular.module('MenuData', []);
+
+})();
---
+++ assignment-4/src/restaurant/service/menu-data.service.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('MenuData')
+.service('MenuDataService', MenuDataService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+// Service to retrieve data from web services rest calls, it's injected on satate definition
+MenuDataService.$inject = ['$q', '$timeout', '$http', 'ApiBasePath']
+function MenuDataService($q, $timeout, $http, ApiBasePath) {
+  var service = this;
+
+  service.getAllCategories = function () {
+    var response = $http({
+      url: (ApiBasePath + "/categories.json")
+    });
+    return response;
+  };
+
+  service.getItemsForCategory = function(categoryShortName) {
+    var response = $http({
+      url: (ApiBasePath + "/menu_items.json"),
+      params: { category: categoryShortName }
+    });
+    return response;
+  };
+
+}
+
+})();
